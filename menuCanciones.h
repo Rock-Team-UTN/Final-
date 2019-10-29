bf34a8f4748d6f84e3a836fcbf21402e21dedf7f
isaiasemanuel22 <56351643+isaiasemanuel22@users.noreply.github.com>
2019-10-29 02:11:11 -0300

    Create menuCanciones.h

---
+++ menuCanciones.h
@@ -0,0 +1,7 @@
+#ifndef MENUCANCIONES_H_INCLUDED
+#define MENUCANCIONES_H_INCLUDED
+#include "ManejoCanciones.h"
+void menuCanciones(nodoArbolCancion * arbol);
+
+
+#endif // MENUCANCIONES_H_INCLUDED
