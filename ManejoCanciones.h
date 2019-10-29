a970ce0da2afb88450493e3ac22d4687ff7a2d19
isaiasemanuel22 <56351643+isaiasemanuel22@users.noreply.github.com>
2019-10-29 11:39:12 -0300

    Update ManejoCanciones.h

--- ManejoCanciones.h
+++ ManejoCanciones.h
@@ -9,5 +9,11 @@ void GuardarCancion (Cancion musica);
 int buscarID(char buscador[]);
 nodoArbolCancion * altaCreado(nodoArbolCancion * arbol,char buscador[]);
 void mostrarCancionesEliminadas(nodoArbolCancion * arbol);
+nodoArbolCancion * bajaCancion(nodoArbolCancion * arbol,char nombre[]);
+void bajarDelArchivo(char nombre[]);
+void mostrarArchivoCancion();
+nodoArbolCancion * modificarCancion(nodoArbolCancion * arbol, int eleccion,nodoArbolCancion * nuevo);
+int contarRegistrosCanciones(int pesoDat);
+
 
 #endif // MANEJOCANCIONES_H_INCLUDED
